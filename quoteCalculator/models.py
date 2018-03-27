e314ff4916213a9c0e352a5127383006679a86e5
Radesh Shiwlochan <RadeshShiwlochan@Radeshs-MBP.fios-router.home>
2018-03-27 00:07:51 -0400

    adds a model for the quoteCalculator app

--- quoteCalculator/models.py
+++ quoteCalculator/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Quote( models.Models ):
+class Quote( models.Model ):
 	address = models.CharField( max_length=200 )
 	cap_rate = models.DecimalField( max_digits=6, decimal_places=2 )
