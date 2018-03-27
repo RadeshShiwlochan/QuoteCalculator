ce35b53967b711bba5d9d0f97d270381e938aae4
radeshshiwlochan <radesh0430@gmail.com>
2018-03-27 01:08:09 -0400

    adds a home page

--- quoteCalculator/views.py
+++ quoteCalculator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Quote
+
+def home( request ):
+	quotes = Quote.objects
+	return render( request, 'quoteCalculator/home.html' , { 'quotes': quotes } )
